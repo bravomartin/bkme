f2b5fb1cd6f68a6aa809425106a9eb11d9530c69
Martin Bravo <bravomartin@gmail.com>
2011-12-05 16:40:30 -0500

    new functions work in progress

--- bkme.rb
+++ bkme.rb
@@ -36,7 +36,6 @@ route = "reports/images/"
  
 track_terms = ['#bkme', '#BKME', '#Bkme']
 
-#now = Time.now.strftime("%I:%M%p")
 
 begin
   back = "Back on track! get me some cars, amigo..."
@@ -53,7 +52,82 @@ end
 
 
 TweetStream::Client.new.track(track_terms) do |status|
-  process_tweet(status)
+
+
+  # get from the object the data we need 
+  user = status.user.screen_name
+  if user != $my_name
+    status_id = status.id
+    user_id = status.user.id
+    hashtags =  status.entities.hashtags
+    entities = status.entities
+    created_at =  status.created_at
+    text = status.text
+    geodata = status.geo
+
+    puts '@'+ user +' says: '+text
+
+    #look for urls
+    #puts entities.inspect
+
+    url = image_url = nil
+    url = find_url(entities)
+    ####    tags = get_tags(hashtags)
+    #look for images
+    image_url = find_media(entities)
+    #keep going only if there is a photo in the report
+    if !url.nil? or !image_url.nil?
+      
+      
+    geodata.nil? ? (address=[nil,nil]) : (get_address(geodata))
+    plate = find_plate(text)
+
+    
+    response = create_response(user, plate, url, geodata, address, tags)
+    
+
+
+    #if there is address and photo, post the info to the database
+    if !geodata.nil? && !url.nil? 
+      tweetdata = {}
+      tweetdata[:tweet_id] = status_id
+      tweetdata[:user_id] = user_id
+      tweetdata[:user_name] = user
+      tweetdata[:text] = text
+      tweetdata[:geolocation] = geodata[:coordinates].join(",")
+      tweetdata[:address] = address
+      if !url.nil?
+        tweetdata[:url] = url
+      end
+      if !image_urls.nil?
+        tweetdata[:image_url] = image_url["media_url"]
+        tweetdata[:local_filename] = image_url["file_route"]
+      end
+      if !plate.nil?
+        tweetdata[:plate] = plate
+      end
+      tweetdata[:created_at] = created_at
+      tweetdata[:response] = status
+      
+      #send data to mongo
+      #authorize credentials
+      auth = $db.authenticate("bkme","youwerebiked1")
+      puts "db authorized." if auth
+      if $reports.find(:tweet_id => status_id).none?
+        $reports.insert(tweetdata)
+        puts "RECORD ADDED"
+      else
+        $reports.update({:tweet_id => status_id}, tweetdata)
+        puts "RECORD UPDATED"
+      end
+       
+    end #if address and photo
+    puts "\nwaiting for a new #bkme...\n\n"
+  end #end if not myself
+
+
+
+
 end
 
 
