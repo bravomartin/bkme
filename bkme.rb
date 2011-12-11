bacbd011c076f09bec12321b703ba499ac20a6c0
Martin Bravo <bravomartin@gmail.com>
2011-12-11 16:17:33 -0500

    new code version 0.3

--- bkme.rb
+++ bkme.rb
@@ -6,8 +6,11 @@
 # require 'bundler'
 # Bundler.require
  
+ 
+waiting = "\nwaiting for a new #bkme...\n\n"
+retries = 0
+last = Time.now
 
-begin
 $LOAD_PATH << './lib'
 
 #clean screen, show art.
@@ -20,133 +23,136 @@ end
 #load local dependencies
 require 'config'
 require 'functions'
+require 'messages'
 
 
 #folder where the images will go
 route = "reports/images/"
 
- $my_name = "bkme"
- last_status = " "
  me = Twitter.verify_credentials()
- $my_name = me["screen_name"] if !me.nil?
+ if !me.nil? then $my_name = me["screen_name"] else $my_name = "bkme" end
  t = Twitter.user_timeline({:count => 1})
- last_status = t[0]["text"] if !t.nil?
- puts "last tweet: " + last_status
+if !t.nil? then last_status = t[0]["text"] else last_status =  " " end 
+ puts "last tweet: " + last_status unless last_status == " "
  
-track_terms = ['#bkme', '#BKME', '#Bkme']
+track_terms = ['#bkme', '#BKME', '#Bkme', '@bkme_ny']
+
+track_terms = ['#test', "photo", "here", "now"] if TEST
 
 
 begin
   back = "Back on track! get me some cars, amigo..."
   if back != last_status
-######Twitter.update(back)
+    Twitter.update(back) unless SAFE
     puts back
   else
-    puts back + " (not sent)"
+    puts "I didnt send the message, but still get me some cars, amigo..."
   end
 rescue Exception => e
   puts e.inspect
   puts "I coudn't send the message, but still get me some cars, amigo..."
 end
 
+begin
+
+puts waiting 
 
 TweetStream::Client.new.on_delete{ |status_id, user_id|  
-  Tweet.delete(status_id)
-}.on_limit { |skip_count|  
-  sleep 10
-}.track(track_terms) do |status|  
+  next
+  }.on_limit { |skip_count|  
+    sleep 10
+  }.track(track_terms) do |status|  
 
-  puts Time.now
-
-  # get from the object the data we need 
   user = status.user.screen_name
-  if user != $my_name
-    status_id = status.id
-    user_id = status.user.id
-    hashtags =  status.entities.hashtags
-    entities = status.entities
-    created_at =  status.created_at
-    text = status.text
-    geodata = status.geo
+  
+  next if status.geo.nil? and TEST
+  
+  #if myself, skip this one
+  if user == $my_name then puts "myself, skipping"; next end
 
-    puts '@'+ user +' says: '+text
+  t = Time.now
+  now = t.strftime("at %I:%M%p %m/%d/%Y")
+  puts "\n\nnew message received from @#{user} #{now}"
+     
+  status_id = status.id
+  user_id = status.user.id
+  hashtags =  status.entities.hashtags
+  entities = status.entities
+  created_at =  status.created_at
+  text = status.text
+  geodata = status.geo
+  
+  
+  puts "@#{user} says: \"#{text}\""
 
-    #look for urls
-    #puts entities.inspect
+  #look for urls
+  url = find_url(entities)
+  tags = nil #get_tags(entities)
+  #look for images
+  image_url = find_media(entities)
 
-    url = image_url = nil
-    url = find_url(entities)
-    ####    tags = get_tags(hashtags)
-    #look for images
-    image_url = find_media(entities)
-    #keep going only if there is a photo in the report
-    if !url.nil? or !image_url.nil?
-      
-      
-    geodata.nil? ? (address=[nil,nil]) : (get_address(geodata))
-    plate = find_plate(text)
+  #keep going only if there is a photo in the report
+  if url.nil? then puts "no url, next #{waiting}"; next end
+    
+  
+  address = get_address(geodata)
+  
+  begin# plate = find_plate(text)
+  response = create_response(user, url, geodata, address, tags)
+
+  options = tweet_options(user_id, geodata)
+
+  send_tweet(response, options) if !response.nil?
+
+
+    if geodata.nil? then puts "no geo, not stored #{waiting}"; next end
+
 
     
-    response = create_response(user, plate, url, geodata, address, tags)
-    
-    send_tweet(response[:status], response[:options])
-    
-
-
-    #if there is address and photo, post the info to the database
-    if !geodata.nil? && !url.nil? 
-      tweetdata = {}
-      tweetdata[:tweet_id] = status_id
-      tweetdata[:user_id] = user_id
-      tweetdata[:user_name] = user
-      tweetdata[:text] = text
-      tweetdata[:geolocation] = geodata[:coordinates].join(",")
-      tweetdata[:address] = address
-      if !url.nil?
-        tweetdata[:url] = url
-      end
-      if !image_urls.nil?
-        tweetdata[:image_url] = image_url["media_url"]
-        tweetdata[:local_filename] = image_url["file_route"]
-      end
-      if !plate.nil?
-        tweetdata[:plate] = plate
-      end
-      tweetdata[:created_at] = created_at
-      tweetdata[:response] = status
+    #store the data in the database
+    tweetdata = {}
+    tweetdata[:tweet_id] = status_id
+    tweetdata[:user_id] = user_id
+    tweetdata[:user_name] = user
+    tweetdata[:text] = text
+    tweetdata[:geolocation] = geodata[:coordinates].join(",")
+    tweetdata[:address] = address
+    tweetdata[:url] = url if !url.nil?
       
-      #send data to mongo
-      #authorize credentials
-      auth = $db.authenticate("bkme","youwerebiked1")
-      puts "db authorized." if auth
-      if $reports.find(:tweet_id => status_id).none?
-        $reports.insert(tweetdata)
-        puts "RECORD ADDED"
-      else
-        $reports.update({:tweet_id => status_id}, tweetdata)
-        puts "RECORD UPDATED"
-      end
-       
-    end #if address and photo
-    puts "\nwaiting for a new #bkme...\n\n"
-  end #end if not myself
+    if !image_url.nil?
+      tweetdata[:image_url] = image_url[:media_url]
+      tweetdata[:local_filename] = image_url[:file_route]
+    end
+    # tweetdata[:plate] = plate if !plate.nil?
+    tweetdata[:created_at] = created_at
+    tweetdata[:response] = status
+    
+    #send data to mongo
+    send_to_mongo(tweetdata)
 
-
-
-
+  rescue Exception => e
+    puts e.message
+  end
+  
+  puts waiting
 end
-
 
 rescue Interrupt => e
   puts "\nProgram finished by the user."
+  
 rescue Exception => e
   puts "somewhere else"
   puts e.message
-  puts e.backtrace
-  error = "problemas! help @brvmrtn!"
-  if error != last_status
-    #Twitter.update(error)
-    puts error
+  retries = 0 if last < Time.now-60*60
+  if retries < 5
+    retries += 1 if last > Time.now-60*10
+    sleep 15*retries
     
+    puts "restarting the Twitter filter for the #{retries.ordinal} time."
+    last = Time.now
+    retry
+  else 
+  Twitter.direct_message_create("brvmrtn", "problemas! #{e.message}"[0,140]) unless SAFE
+    puts "Asking for help!"
   end
 end
--- lib/config.rb
+++ lib/config.rb
@@ -1,13 +1,24 @@
+#global modes
+DEBUG = false
+SAFE = true
+TEST = true
+
+puts "***Running in safe mode, nothing will be actually stored or sent***" if SAFE
+puts "***Running in debug mode, using @bkmetst credentials***" if DEBUG
+puts "***Running a test with random values and keywords***" if TEST
 
 begin
 require 'rubygems'
 require 'tweetstream'
+#gem 'twitter',  '=1.7.2'
 require 'twitter'
 require 'mongo'
+
 
 require 'net/http'
 require 'net/https'
 require 'JSON'
+require 'time'
 
 #connect to AWS S3
 # AWS::S3::Base.establish_connection!(
@@ -19,24 +30,25 @@ require 'JSON'
 #connect to mongo
   $db = Mongo::Connection.new("dbh84.mongolab.com", 27847).db("bkme")
 
-
-#production keys
-CONSUMER_KEY = 'ocnQkTD0dYfD7o2elj2Og'
-CONSUMER_SECRET = 'RDu2tk6kzbXjQtNlH07QYJjpkENQ7NUdstfl2THloU'
-OAUTH_TOKEN = '397570607-vm9Se5BnZVkblyUNeJwsx1ftFMKQ4ftIlgMwpUpK'
-OAUTH_TOKEN_SECRET = 'Vf8tA3ujoVYTLmgr5reiDsDHCbEI40yRjMmij0JZO0'
-
-#test keys
-# CONSUMER_KEY = 'Ce8Ciu1HXAEBXBg1O3qaw'
-# CONSUMER_SECRET = 'gPHYdkzmWHsdF1vuoyDcLvkGEgPYHODAvmFS3cgww'
-# OAUTH_TOKEN = '407596227-Xg2zSIs3jmTaBaVCxv7aIH970B6OVd5v1G7izhev'
-# OAUTH_TOKEN_SECRET = 'f55gs6THCZuMxUW6aNtuLxC8Mw1vS9ushKsXXmyn4vM'
-
-
+if DEBUG
+  #bkmetst keys
+  CONSUMER_KEY = 'Ce8Ciu1HXAEBXBg1O3qaw'
+  CONSUMER_SECRET = 'gPHYdkzmWHsdF1vuoyDcLvkGEgPYHODAvmFS3cgww'
+  OAUTH_TOKEN = '407596227-Xg2zSIs3jmTaBaVCxv7aIH970B6OVd5v1G7izhev'
+  OAUTH_TOKEN_SECRET = 'f55gs6THCZuMxUW6aNtuLxC8Mw1vS9ushKsXXmyn4vM'
+else
+  #bkme_ny keys
+  CONSUMER_KEY = 'ocnQkTD0dYfD7o2elj2Og'
+  CONSUMER_SECRET = 'RDu2tk6kzbXjQtNlH07QYJjpkENQ7NUdstfl2THloU'
+  OAUTH_TOKEN = '397570607-vm9Se5BnZVkblyUNeJwsx1ftFMKQ4ftIlgMwpUpK'
+  OAUTH_TOKEN_SECRET = 'Vf8tA3ujoVYTLmgr5reiDsDHCbEI40yRjMmij0JZO0'
+end
 
   #load collection to insert reports in
   $reports = $db.collection("records")
   auth = $db.authenticate("bkme","youwerebiked1")
+  puts "Database authenticated successfully" if auth
+
 
 #authtenticating the TweetStream object
 TweetStream.configure do |config|
@@ -46,7 +58,9 @@ TweetStream.configure do |config|
   config.oauth_token_secret = OAUTH_TOKEN_SECRET
   config.auth_method = :oauth
   config.parser   = :yajl
+  
 end
+
 # authenticating the Twitter object
 Twitter.configure do |config|
   config.consumer_key = CONSUMER_KEY
@@ -54,8 +68,8 @@ Twitter.configure do |config|
   config.oauth_token = OAUTH_TOKEN
   config.oauth_token_secret = OAUTH_TOKEN_SECRET
   puts "Twitter authenticated successfully"
+  
 end
-
 
 rescue Exception => e
   puts "in the config"
